ff1705aaf80a8e2d0df7dd216db9f9550cde0dd3
AbiduDaGeek <50393217+AbiduDaGeek@users.noreply.github.com>
2020-02-29 04:13:06 +0600

    PlayerMovement Added
    NavMeshBaked.
    Ground Is LayerMasked as ground from code

---
+++ Skeleton_RPG/Assets/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    //movementMask is used to define in which places player can go
+    public LayerMask movementMask;
+    //reference to camera, stored in a variable
+    Camera cam;
+    PlayerMotor motor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //getting the camera, initialization
+        cam = Camera.main ;
+        motor = GetComponent<PlayerMotor>();
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            //Declaring the ray to be shooted from the cam
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            //declare the variable to store the info, what we hit ?
+            RaycastHit hit;
+            //cast ray from camera to screen position
+            if(Physics.Raycast(ray,out hit, 100, movementMask))
+            {
+                //move player to what we hit
+                motor.MoveToPoint(hit.point);
+                //difoucing
+            }
+        }
+        
+    }
+}
---
+++ Skeleton_RPG/Assets/PlayerMotor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class PlayerMotor : MonoBehaviour
+{
+    //Reffrence to NavMesh agent
+    NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Getting the Navmesh agent component
+        agent = GetComponent<NavMeshAgent>();
+        
+    }
+
+public void MoveToPoint( Vector3 point )
+{
+    agent.SetDestination(point) ;
+
+}
+
+}
